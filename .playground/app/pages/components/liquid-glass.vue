2b4194d6280728298e51bc1cdcd7b0d970de3d10
Type-32 <87076491+Type-32@users.noreply.github.com>
2026-02-22 23:40:28 +0800

    feat: add LiquidGlass component from Inspira UI with playground page

---
+++ .playground/app/pages/components/liquid-glass.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<DashboardContentPanel title="LiquidGlass">
+		<UContainer>
+			<DashboardContentPage
+				title="UeLiquidGlass"
+				description="The LiquidGlass component from Inspira UI: https://inspira-ui.com/docs/en/components/visualization/liquid-glass"
+				sourceUrl="/components/Ue/LiquidGlass.vue"
+			>
+				<UeLiquidGlass class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+					Preview Text
+				</UeLiquidGlass>
+			</DashboardContentPage>
+		</UContainer>
+	</DashboardContentPanel>
+</template>
+
+<style scoped>
+
+</style>
