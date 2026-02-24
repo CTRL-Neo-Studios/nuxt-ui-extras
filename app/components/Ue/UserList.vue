86b2f1d439b03bc9db817aea93fc0850bb857630
Type-32 <87076491+Type-32@users.noreply.github.com>
2026-02-24 16:41:28 +0800

    feat: add UeUserList component

---
+++ app/components/Ue/UserList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type {AvatarProps} from "@nuxt/ui";
+
+const props = withDefaults(defineProps<{
+	data: AvatarProps[],
+	class?: string
+}>(), {})
+</script>
+
+<template>
+	<div class="flex flex-col" :class="props.class">
+		<template v-for="(user, index) in data" :key="index">
+			<UUser v-bind="user"/>
+		</template>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
